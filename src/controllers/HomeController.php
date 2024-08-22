673686ed207e3c2e9d12b958e1cb001c3a2da517
Gabriel <suppershock777@gmail.com>
2024-08-22 09:58:43 -0300

    Feat: Detectado rotas de login & verificação de logins

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -2,19 +2,23 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
 class HomeController extends Controller {
 
+    // vai armazenar o usuario que está logado
+    private $loggedUser;
+
+    // primeiro que vai executar e vai verificar se o user está logado
+    public function __construct(){
+        $this->loggedUser = LoginHandler::checkLogin();
+        if($this->loggedUser === false){
+            $this->redirect('/login');
+        }
+    }
+    
     public function index() {
         $this->render('home', ['nome' => 'Bonieky']);
-    }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
     }
 
 }
